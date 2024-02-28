4583a61c9e8650cf6ed5b4150d7cea714172388d
nehanumolu <nanumolu@andrew.cmu.edu>
2024-02-28 18:03:50 -0500

    renaming javascript files

--- solution-hw5/js/cart_page.js
+++ solution-hw5/js/cart_page.js
@@ -1,52 +1,5 @@
-const rolls = {
-    "Original": {
-        "basePrice": 2.49,
-        "imageFile": "original-cinnamon-roll.jpg"
-    },
-    "Apple": {
-        "basePrice": 3.49,
-        "imageFile": "apple-cinnamon-roll.jpg"
-    },
-    "Raisin": {
-        "basePrice": 2.99,
-        "imageFile": "raisin-cinnamon-roll.jpg"
-    },
-    "Walnut": {
-        "basePrice": 3.49,
-        "imageFile": "walnut-cinnamon-roll.jpg"
-    },
-    "Double-Chocolate": {
-        "basePrice": 3.99,
-        "imageFile": "double-chocolate-cinnamon-roll.jpg"
-    },
-    "Strawberry": {
-        "basePrice": 3.99,
-        "imageFile": "strawberry-cinnamon-roll.jpg"
-    }    
-};
 
 
-// Code for product detail page --> displaying product
-// based on url parameters
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get('roll');
-
-const base_price = rolls[rollType].basePrice;
-const imagePath = rolls[rollType].imageFile;
-const rollName = rollType;
-
-const header = document.querySelector("#banner");
-header.innerText = rollName + ' Cinnamon Roll';
-
-const image = document.querySelector(".product-image");
-image.src = "assets/products/" + imagePath;
-
-const price = document.querySelector('.cart-price');
-price.innerText = "$" + base_price.toFixed(2);
-
-
-// Code for Cart page
 let cart = [] // initialize cart array
 
 // Roll class 
@@ -59,8 +12,8 @@ class Roll {
     }
 }
 
-// wrote a function that would comptue price given pack_size, roll_type, glazing
-function computePrice(pack_size, roll_type, glazing) {
+// Compute a roll's total price from pack size, roll type and glazing
+function computeRollPrice(roll_type, glazing, pack_size) {
     // get base price of roll type
     let base_price = rolls[roll_type].basePrice;
 
@@ -69,7 +22,8 @@ function computePrice(pack_size, roll_type, glazing) {
     let price_adaptation = packSize.priceAdaptation;
 
     // find glazing price for given glazing
-    let glazing_lowercase = glazing.toLowerCase().replace(' ', '-');
+    let glazing_lowercase = glazing.toLowerCase().replace(/ /g, '-');
+    console.log(glazing_lowercase)
     let glaze = glazePrices.find(glazePrice => glazePrice.glaze == glazing_lowercase);
     let glazePrice = glaze.price;
 
@@ -78,11 +32,12 @@ function computePrice(pack_size, roll_type, glazing) {
     return total_price;
 }
 
-// create four new roll instances
-let original_roll = new Roll("Original", "Sugar Milk", 1, computePrice(1, "Original", "Sugar Milk"))
-let walnut_roll = new Roll("Walnut", "Vanilla Milk", 12, computePrice(12, "Walnut", "Vanilla Milk"))
-let raisin_roll = new Roll("Raisin", "Sugar Milk", 3, computePrice(3, "Raisin", "Sugar Milk"))
-let apple_roll = new Roll("Apple", "Original", 3, computePrice(3, "Apple", "Original"))
+// Using Roll class, make four new Roll objects and add them to your cart
+
+let original_roll = new Roll("Original", "Sugar Milk", 1, computeRollPrice("Original", "Sugar Milk", 1))
+let walnut_roll = new Roll("Walnut", "Vanilla Milk", 12, computeRollPrice("Walnut", "Vanilla Milk", 12))
+let raisin_roll = new Roll("Raisin", "Sugar Milk", 3, computeRollPrice("Raisin", "Sugar Milk", 3))
+let apple_roll = new Roll("Apple", "Original", 3, computeRollPrice("Apple", "Original", 3))
 
 // add new roll instances to cart array
 cart.push(original_roll, walnut_roll, raisin_roll, apple_roll);
@@ -150,19 +105,4 @@ updateCart(walnut_roll);
 updateCart(raisin_roll);
 updateCart(apple_roll);
 
-function addToCart() {
-    const glazingSelect = document.querySelector("#glazing");
-    const glazing = glazePrices[glazingSelect.selectedIndex].glaze;
-
-    const packSizeSelect = document.querySelector("#pack-size");
-    const packSize = parseInt(packSizeSelect.options[packSizeSelect.selectedIndex].text);
-
-    const newRoll = new Roll(rollType, glazing, packSize, base_price);
-    cart.push(newRoll);
-
-    console.log(cart);
-}
-
-const addToCartBtn = document.querySelector("#add-to-cart-btn");
-addToCartBtn.addEventListener('click', addToCart);
 
---
+++ solution-hw5/js/detail_page.js
@@ -0,0 +1,62 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+
+// displaying product based on url parameters
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+
+const base_price = rolls[rollType].basePrice;
+const imagePath = rolls[rollType].imageFile;
+const rollName = rollType;
+
+const header = document.querySelector("#banner");
+header.innerText = rollName + ' Cinnamon Roll';
+
+const image = document.querySelector(".product-image");
+image.src = "assets/products/" + imagePath;
+
+const price = document.querySelector('.cart-price');
+price.innerText = "$" + base_price.toFixed(2);
+
+
+function addToCart() {
+    const glazingSelect = document.querySelector("#glazing");
+    const glazing = glazePrices[glazingSelect.selectedIndex].glaze;
+
+    const packSizeSelect = document.querySelector("#pack-size");
+    const packSize = parseInt(packSizeSelect.options[packSizeSelect.selectedIndex].text);
+
+    const newRoll = new Roll(rollType, glazing, packSize, base_price);
+    cart.push(newRoll);
+
+    console.log(cart);
+}
+
+const addToCartBtn = document.querySelector("#add-to-cart-btn");
+addToCartBtn.addEventListener('click', addToCart);
