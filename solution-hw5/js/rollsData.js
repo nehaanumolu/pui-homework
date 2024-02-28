c8bcaad32953ee213c594387c181572d0d212e02
nehanumolu <nanumolu@andrew.cmu.edu>
2024-02-28 17:24:56 -0500

    create new updateCart function()

--- solution-hw5/js/rollsData.js
+++ solution-hw5/js/rollsData.js
@@ -83,9 +83,68 @@ let apple_roll = new Roll("Apple", "Original", 3, computePrice(3, "Apple", "Orig
 // add new roll instances to cart array
 cart.push(original_roll, walnut_roll, raisin_roll, apple_roll);
 
-function displayCart(Roll) {
-    
+function updateCart(roll) {
+    const cartWrapper = document.querySelector('.product-cart-wrapper');
+
+    // create a new roll listing
+    const newRoll = document.createElement('div');
+    newRoll.classList.add('item-listing');
+
+    // Item Image
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('item-image');
+
+    const productImage = document.createElement('img');
+    productImage.classList.add('product-image');
+    let productImagePath = "assets/products/" + rolls[roll.type].imageFile;
+    productImage.src = productImagePath;
+
+    const removeButton = document.createElement('p');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
+
+    imageDiv.appendChild(productImage);
+    imageDiv.appendChild(removeButton);
+
+    // Item details
+    const itemDetailsDiv = document.createElement('div');
+    itemDetailsDiv.classList.add('item-details');
+    const itemName = document.createElement('p');
+    itemName.textContent = roll.type + " Cinnamon Roll";
+    const itemGlazing = document.createElement('p');
+    itemGlazing.textContent = "Glazing: " + roll.glazing;
+    const itemPackSize = document.createElement('p');
+    itemPackSize.textContent = "Pack Size: " + roll.size;
+    itemDetailsDiv.appendChild(itemName);
+    itemDetailsDiv.appendChild(itemGlazing);
+    itemDetailsDiv.appendChild(itemPackSize);
+
+    // Item Price
+    const itemPriceDiv = document.createElement('div');
+    itemPriceDiv.classList.add('item-price');
+    const price = document.createElement('p');
+    price.textContent = "$ " + computePrice(roll.size, roll.type, roll.glazing).toFixed(2);
+    itemPriceDiv.appendChild(price);
+
+    // append all to new Roll
+    newRoll.appendChild(imageDiv);
+    newRoll.appendChild(itemDetailsDiv);
+    newRoll.appendChild(itemPriceDiv);
+
+    // append newRoll to cart
+    cartWrapper.appendChild(newRoll);
+    displayPrice();
+
+    removeButton.addEventListener('click', function() {
+        cartWrapper.removeChild(newRoll);
+        displayPrice();
+    });
 }
+
+displayCart(original_roll);
+displayCart(walnut_roll);
+displayCart(raisin_roll);
+displayCart(apple_roll);
 
 function addToCart() {
     const glazingSelect = document.querySelector("#glazing");
